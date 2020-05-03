e3f55fa5a15962f48c15c9805ce2930ae0401d77
Andrii Stasiuk <43143376+andrii-stasiuk@users.noreply.github.com>
2020-05-03 21:09:56 +0200

    GuestList App
    Programming test. You have to design and develop an application for the director of Company. He wants something to help him organise the guest list for a large party. The application should show who he has invited and who has responded.

---
+++ GuestList/GuestList/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GuestList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(656, 252);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Import List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Export List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(411, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(541, 292);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Number of Guests";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter the person\'s name:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(93, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 292);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Delete Person(s)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 321);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Guest List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ GuestList/GuestList/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuestList
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable("tbl");
+        string workingFile = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Method for Import XML data from file
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Guest list file|*.xml";
+            openFileDialog1.Title = "Import guest list file";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    //table.ReadXml(Application.StartupPath + "\\guests.xml"); old version, testing purposes
+                    table.Clear();
+                    workingFile = openFileDialog1.FileName;
+                    table.ReadXml(workingFile);
+                    dataGridView1.DataSource = table;
+                    MessageBox.Show("Guest list successfully imported");
+                 }
+                 catch
+                 {
+                    MessageBox.Show("An error occurred while importing");
+                 }
+        }
+
+        //Method for export XML data to file
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //table.WriteXml(@"d:\guests.xml", XmlWriteMode.WriteSchema); old version, testing purposes
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Guest list file|*.xml";
+            saveFileDialog1.Title = "Export guest list file";
+            saveFileDialog1.DefaultExt = "xml";
+            saveFileDialog1.FileName = "*.xml";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    //table.WriteXml(Application.StartupPath + "\\guests.xml", XmlWriteMode.WriteSchema); old version, testing purposes
+                    workingFile = saveFileDialog1.FileName;
+                    table.WriteXml(workingFile, XmlWriteMode.WriteSchema);
+                    MessageBox.Show("Guest list successfully exported");
+                }
+                catch
+                {
+                    MessageBox.Show("An error occurred while exporting");
+                }
+        }
+
+        //Method that creates table structure for user data (guest list)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var colName = new DataGridViewTextBoxColumn();
+            var colSex = new DataGridViewComboBoxColumn();
+            var colInvited = new DataGridViewCheckBoxColumn();
+            var colResponded = new DataGridViewCheckBoxColumn();
+
+            dataGridView1.Columns.Add(colName);
+            dataGridView1.Columns.Add(colSex);
+            dataGridView1.Columns.Add(colInvited);
+            dataGridView1.Columns.Add(colResponded);
+
+            colName.HeaderText = "Person Name";
+            colName.DataPropertyName = "NameField";
+
+            colSex.HeaderText = "Sex";
+            colSex.DataPropertyName = "SexField";
+            colSex.Items.Add("male");
+            colSex.Items.Add("female");
+
+            colInvited.HeaderText = "Invited";
+            colInvited.DataPropertyName = "InvitedField";
+
+            colResponded.HeaderText = "Responded";
+            colResponded.DataPropertyName = "RespondedField";
+
+            table.Columns.Add(new DataColumn("NameField", typeof(string)));
+            table.Columns.Add(new DataColumn("SexField", typeof(string)));
+            table.Columns.Add(new DataColumn("InvitedField", typeof(bool)));
+            table.Columns.Add(new DataColumn("RespondedField", typeof(bool)));
+            //table.Columns.Add(new DataColumn("ID", typeof(int))); reserved for future purposes
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = table.DefaultView;
+        }
+        
+        //Method for searching persons by their names in list
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string search = textBox1.Text;
+            bool notFoundMarker = true;
+
+            dataGridView1.CurrentRow.Selected = false;
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                //if (dataGridView1.Rows[i].Cells[0].Value.ToString().Contains(search)) old version of search - case sensitive
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString().IndexOf(search, StringComparison.OrdinalIgnoreCase) >=0)
+                {
+                    dataGridView1.Rows[i].Selected = true;
+                    notFoundMarker = false;
+                    break;
+                }
+            }
+
+            if (notFoundMarker) MessageBox.Show("A person with this name was not found");
+        }
+
+        //Method that checks the number of men and women who will come to the party
+        //and advises what should the director do to achieve better results
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int man = 0;
+            int woman = 0;
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                if( (dataGridView1.Rows[i].Cells[1].Value.ToString() == "male") && (dataGridView1.Rows[i].Cells[3].Value.Equals(true)))
+                    man++;
+                else if( (dataGridView1.Rows[i].Cells[1].Value.ToString() == "female") && (dataGridView1.Rows[i].Cells[3].Value.Equals(true)))
+                    woman++;
+            }
+
+            if(man>woman)
+                MessageBox.Show("You need to invite more women: " + man.ToString() + " men and " + woman.ToString() + " women will come");
+            else if(woman>man)
+                MessageBox.Show("You need to invite more men: " + man.ToString() + " men and " + woman.ToString() + " women will come");
+            else
+                MessageBox.Show("You are doing everything right! The same number of men and women will come: " + man.ToString() + " men and " + woman.ToString()+ " women");
+        }
+
+        //Method for saving XML data to file
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (workingFile != "")
+            {
+                try
+                {
+                    table.WriteXml(workingFile, XmlWriteMode.WriteSchema);
+                    MessageBox.Show("Guest list successfully saved to file " + workingFile);
+                }
+                catch
+                {
+                    MessageBox.Show("An error occurred while saving");
+                }
+            }
+            else button2.PerformClick();
+
+        }
+
+        //Method that deletes one person or group of persons
+        private void button6_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.RemoveAt(row.Index);
+            }
+        }
+    }
+}
